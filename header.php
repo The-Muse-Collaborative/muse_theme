9828fc1885df72a94ca392130e0ebc6708124218
Joel Smith <jsumnersmith@gmail.com>
2018-03-22 21:44:01 -0400

    Added google fonts fallbacks for fonts.

--- header.php
+++ header.php
@@ -20,8 +20,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link rel="stylesheet" href="https://use.typekit.net/zbs3oue.css">
 	<?php wp_head(); ?>
+	<link href="https://fonts.googleapis.com/css?family=Exo+2:500,500i,700|Montserrat:400,400i,700" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.typekit.net/zbs3oue.css">
 </head>
 
 <body <?php body_class(); ?>>
