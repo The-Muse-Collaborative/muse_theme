a3bec2429422b052cdca18440f78420a18f014d1
Michael Zier <michael@themusecollaborative.org>
2018-05-03 13:40:38 -0400

    Creates a display option for the album custom post type

--- inc/custom-taxonomies.php
+++ inc/custom-taxonomies.php
@@ -41,6 +41,8 @@ add_action( 'init', 'bio_init' );
 /****************************************/
 /* Creates custom taxonomy for articles */
 /****************************************/
+
+// creates hierarchical categories for articles
 function article_init() {
 	$labels = array(
 		'name'              => _x( 'Articles', 'taxonomy general name', 'textdomain' ),
@@ -67,9 +69,40 @@ function article_init() {
 	);
 
 	register_taxonomy('article', array( 'article' ), $args);
-}
 
+// creates non-hierarchical tags for articles
+	$labels = array(
+		'name'              				 => _x( 'Article Tags', 'taxonomy general name', 'textdomain' ),
+		'singular_name'    					 => _x( 'Article Tag', 'taxonomy singular name', 'textdomain' ),
+		'search_items'     					 => __( 'Search Article Tags', 'textdomain' ),
+		'all_items'        					 => __( 'All Article Tags', 'textdomain' ),
+		'parent_item'      					 => null,
+		'parent_item_colon'					 => null,
+		'edit_item'        					 => __( 'Edit Article Tags', 'textdomain' ),
+		'separate_items_with_commas' => __( 'Separate tags with commas' ),
+		'add_or_remove_items'				 => __( 'Add or remove Tags' ),
+		'choose_from_most_used'			 => __( 'Choose from most used Tags'),
+		'update_item'      					 => __( 'Update Article Tags', 'textdomain' ),
+		'add_new_item'     					 => __( 'Add New Article Tag', 'textdomain' ),
+		'new_item_name'     				 => __( 'New Article Name', 'textdomain' ),
+		'not_found'									 => __( 'No tags found'),
+		'menu_name'        					 => __( 'Article Tags', 'textdomain' ),
+	);
+
+	$args = array(
+		'hierarchical'     		  => false,
+		'labels'           		  => $labels,
+		'show_ui'          		  => true,
+		'public'           		  => true,
+		'show_admin_column' 		=> true,
+		'update_count_callback'	=> '_update_post_term_count',
+		'query_var'       		  => true,
+		'rewrite'          		  => array( 'slug' => 'tag' ),
+	);
+
+	register_taxonomy('tag', array( 'article' ), $args);
+}
 /*************************************/
 /* Runs custom taxonomy for articles */
 /*************************************/
-add_action( 'init', 'article_init' );
+add_action( 'init', 'article_init', 0 );
