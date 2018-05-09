c464e80b95527470108e6e5c709c0ea8de3a15fb
Michael Zier <michael@themusecollaborative.org>
2018-05-09 19:55:25 -0400

    Further custom post type and custom data fields added with appropriate template pages. & some 404 error changes.

--- functions.php
+++ functions.php
@@ -5,77 +5,52 @@
  * @package understrap
  */
 
-/**
- * Initialize theme default settings
- */
+/** Initialize theme default settings **/
 require get_template_directory() . '/inc/theme-settings.php';
 
-/**
- * Initialize custom post settings
- */
+/** Initialize custom post settings **/
 require get_template_directory() . '/inc/custom-posts.php';
 
-/**
- * Initialize custom post settings
- */
+/** Initialize custom post settings **/
 require get_template_directory() . '/inc/custom-taxonomies.php';
 
-/**
- * Theme setup and custom theme supports.
- */
+/** Theme setup and custom theme supports. **/
 require get_template_directory() . '/inc/setup.php';
 
-/**
- * Register widget area.
- */
+/** Register widget area. **/
 require get_template_directory() . '/inc/widgets.php';
 
-/**
- * Enqueue scripts and styles.
- */
+/** Enqueue scripts and styles. **/
 require get_template_directory() . '/inc/enqueue.php';
 
-/**
- * Custom template tags for this theme.
- */
+/** Custom template tags for this theme. **/
 require get_template_directory() . '/inc/template-tags.php';
 
-/**
- * Custom template tags for this theme.
- */
+/** Custom template tags for this theme. **/
 require get_template_directory() . '/inc/pagination.php';
 
-/**
- * Custom functions that act independently of the theme templates.
- */
+/** Custom functions that act independently of the theme templates. **/
 require get_template_directory() . '/inc/extras.php';
 
-/**
- * Customizer additions.
- */
+/** Customizer additions. **/
 require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Custom Comments file.
- */
+/** Custom Comments file. **/
 require get_template_directory() . '/inc/custom-comments.php';
 
-/**
- * Load Jetpack compatibility file.
- */
+/** Load Jetpack compatibility file. **/
 require get_template_directory() . '/inc/jetpack.php';
 
-/**
- * Load custom WordPress nav walker.
- */
+/** Load custom WordPress nav walker. **/
 require get_template_directory() . '/inc/bootstrap-wp-navwalker.php';
 
-/**
- * Load WooCommerce functions.
- */
+/** Load WooCommerce functions. **/
 require get_template_directory() . '/inc/woocommerce.php';
 
-/**
- * Load Editor functions.
- */
+/** Load Editor functions. **/
 require get_template_directory() . '/inc/editor.php';
+
+/** CURRENTLY INACTIVE FUNCTIONS **/
+
+/** Load custom file-types for media folder. **/
+//require get_template_directory() . '/inc/custom-media-files.php';
---
+++ inc/custom-media-files.php
@@ -0,0 +1,8 @@
+<?php
+/**       Adds additional filetype permissions to the media folder if needed       **/
+
+function my_myme_types($mime_types){
+    $mime_types['svg'] = 'image/svg+xml';
+    return $mime_types;
+}
+add_filter('upload_mimes', 'my_myme_types', 1, 1);
