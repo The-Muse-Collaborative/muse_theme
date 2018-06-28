4d6a3dd209417c6863fb924e70873dafe26575b9
Joel Smith <jsumnersmith@gmail.com>
2018-06-27 20:45:56 -0400

    Styled blog list and show page.

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -8,43 +8,43 @@
 ?>
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+	<div class="row">
+		<header class="entry-header mc-excerpt-header col-md-4">
 
-	<header class="entry-header">
+			<?php the_title( sprintf( '<h4 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+			'</a></h4>' ); ?>
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-		'</a></h2>' ); ?>
+			<?php if ( 'post' == get_post_type() ) : ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
+				<div class="entry-meta">
+					<?php understrap_posted_on(); ?>
+				</div><!-- .entry-meta -->
 
-			<div class="entry-meta">
-				<?php understrap_posted_on(); ?>
-			</div><!-- .entry-meta -->
+			<?php endif; ?>
 
-		<?php endif; ?>
+		</header><!-- .entry-header -->
 
-	</header><!-- .entry-header -->
+		<div class="col-md-1"></div>
 
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+		<div class="entry-content mc-excerpt-content col-md-7">
+			<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
-	<div class="entry-content">
+				<?php the_content(); ?>
 
-		<?php
-		the_excerpt();
-		?>
+			<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			) );
+			?>
 
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-			'after'  => '</div>',
-		) );
-		?>
 
-	</div><!-- .entry-content -->
+			<footer class="entry-footer">
 
-	<footer class="entry-footer">
+			<?php understrap_entry_footer(); ?>
 
-		<?php understrap_entry_footer(); ?>
-
-	</footer><!-- .entry-footer -->
+			</footer><!-- .entry-footer -->
+			</div><!-- .entry-content -->
+		</div>
 
 </article><!-- #post-## -->
