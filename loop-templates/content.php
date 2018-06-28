a91d09177e590ba906c807f7c2f408e5ae10aee3
Joel Smith <jsumnersmith@gmail.com>
2018-06-27 21:02:57 -0400

    Updated grid placement as to better take advantage of bootstrap padding.

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -9,42 +9,43 @@
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 	<div class="row">
-		<header class="entry-header mc-news-header col-md-4">
+		<div class="col-md-4">
+			<header class="entry-header mc-news-header">
 
-			<?php the_title( sprintf( '<h4 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-			'</a></h4>' ); ?>
+				<?php the_title( sprintf( '<h4 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+				'</a></h4>' ); ?>
 
-			<?php if ( 'post' == get_post_type() ) : ?>
+				<?php if ( 'post' == get_post_type() ) : ?>
 
-				<div class="entry-meta">
-					<?php understrap_posted_on(); ?>
-				</div><!-- .entry-meta -->
+					<div class="entry-meta">
+						<?php understrap_posted_on(); ?>
+					</div><!-- .entry-meta -->
 
-			<?php endif; ?>
+				<?php endif; ?>
 
-		</header><!-- .entry-header -->
+			</header><!-- .entry-header -->
+		</div>
+		<div class="col-md-8">
+			<div class="entry-content mc-news-content">
+				<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
-		<div class="col-md-1"></div>
+					<?php the_content(); ?>
 
-		<div class="entry-content mc-news-content col-md-7">
-			<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
-				<?php the_content(); ?>
-
-			<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-				'after'  => '</div>',
-			) );
-			?>
+				<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+					'after'  => '</div>',
+				) );
+				?>
 
 
-			<footer class="entry-footer">
+				<footer class="entry-footer">
 
-			<?php understrap_entry_footer(); ?>
+				<?php understrap_entry_footer(); ?>
 
-			</footer><!-- .entry-footer -->
-			</div><!-- .entry-content -->
+				</footer><!-- .entry-footer -->
+				</div><!-- .entry-content -->
+			</div>
 		</div>
 
 </article><!-- #post-## -->
