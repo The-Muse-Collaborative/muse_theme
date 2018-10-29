0ad2f891b49c911175547faf4596840d659beea2
Michael Zier <michael@themusecollaborative.org>
2018-10-29 18:44:45 -0400

    Ordered index.scss & fixed some breakpoint issues

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<footer class="mc-footer mc-top-footer" style="background-image: url(<?php echo get_template_directory_uri() . '/images/top-footer-background.jpg'; ?>)">
 		<div class="<?php echo esc_attr( $container ); ?>">
 			<div class="row">
-				<div class="col-md-5 col-sm-12 mc-top-footer--right contact_bg" style="background-image: url(<?php echo get_template_directory_uri() . '/images/top-footer-overlay.png'; ?>)">
+				<div class="col-md-4 col-sm-12 mc-top-footer--right contact_bg" style="background-image: url(<?php echo get_template_directory_uri() . '/images/top-footer-overlay.png'; ?>)">
 					<h3 class="white">Contact</h3>
 					<div class="mc-top-footer--item">
 						<p>
