bae9497b3878f192c112b9783ffd02a3e4d69fab
Joel Smith <jsumnersmith@gmail.com>
2018-11-07 21:03:34 -0500

    Added stubbed-in schema file to use alpaca

---
+++ js/schema.js
@@ -0,0 +1,374 @@
+var schemaTest = {
+    "schema": {
+      "type": "object",
+      "required": [
+        "personal_info",
+        "billing_address",
+        "donation_info"
+      ],
+      "additionalProperties": false,
+      "properties": {
+        "personal_info": {
+          "title": "Personal Information",
+          "type": "object",
+          "required": [
+            "email",
+            "first_name",
+            "last_name"
+          ],
+          "additionalProperties": false,
+          "properties": {
+            "email": {
+              "title": "Email Address",
+              "type": "string",
+              "format": "email",
+              "minLength": 5,
+              "maxLength": 254,
+              "pattern": "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$"
+            },
+            "first_name": {
+              "title": "First Name",
+              "type": "string",
+              "minLength": 1,
+              "maxLength": 512
+            },
+            "last_name": {
+              "title": "Last Name",
+              "type": "string",
+              "minLength": 1,
+              "maxLength": 512
+            }
+          }
+        },
+        "billing_address": {
+          "title": "Billing Address",
+          "type": "object",
+          "required": [
+            "address_line_1",
+            "city",
+            "state",
+            "zip_code"
+          ],
+          "additionalProperties": false,
+          "properties": {
+            "address_line_1": {
+              "title": "Address",
+              "type": "string",
+              "minLength": 3,
+              "maxLength": 512
+            },
+            "address_line_2": {
+              "type": "string",
+              "minLength": 1,
+              "maxLength": 512
+            },
+            "city": {
+              "title": "City",
+              "type": "string",
+              "minLength": 1,
+              "maxLength": 512
+            },
+            "state": {
+              "title": "State",
+              "type": "string",
+              "enum": [
+                "AL",
+                "AK",
+                "AS",
+                "AZ",
+                "AR",
+                "AE",
+                "AP",
+                "AA",
+                "CA",
+                "CO",
+                "CT",
+                "DE",
+                "DC",
+                "FM",
+                "FL",
+                "GA",
+                "GU",
+                "HI",
+                "ID",
+                "IL",
+                "IN",
+                "IA",
+                "KS",
+                "KY",
+                "LA",
+                "ME",
+                "MH",
+                "MD",
+                "MA",
+                "MI",
+                "MN",
+                "MS",
+                "MO",
+                "MT",
+                "NE",
+                "NV",
+                "NH",
+                "NJ",
+                "NM",
+                "NY",
+                "NC",
+                "ND",
+                "MP",
+                "OH",
+                "OK",
+                "OR",
+                "PW",
+                "PA",
+                "PR",
+                "RI",
+                "SC",
+                "SD",
+                "TN",
+                "TX",
+                "UT",
+                "VT",
+                "VI",
+                "VA",
+                "WA",
+                "WV",
+                "WI",
+                "WY"
+              ]
+            },
+            "zip_code": {
+              "title": "Zip Code",
+              "type": "string",
+              "pattern": "^[0-9]{5}(\\-[0-9]{4})?$"
+            }
+          }
+        },
+        "donation_info": {
+          "title": "Donation Details",
+          "type": "object",
+          "required": [
+            "amount",
+            "recurring",
+            "stripe_token"
+          ],
+          "additionalProperties": false,
+          "properties": {
+            "amount": {
+              "title": "Donation Amount",
+              "type": "number",
+              "enum": [5.00, 10.00, 25.00, 0.00]
+            },
+            "custom_amount": {
+              "title": "Custom Donation Amount",
+              "type": "number",
+              "minimum": 1,
+              "maximum": 999999.99,
+              "multipleOf": 0.01
+            },
+            "recurring": {
+              "type": "boolean"
+            },
+            "frequency": {
+              "title": "Donation Frequency",
+              "type": "string",
+              "default": "monthly",
+              "enum": [
+                "monthly",
+                "bimonthly",
+                "annually"
+              ]
+            },
+            "first_charge": {
+              "title": "First Donation Date",
+              "type": "string",
+              "format": "date"
+            },
+            "stripe_token": {
+              "title": "Credit/Debit Card",
+              "type": "string",
+              "pattern": "^tok_[a-zA-Z0-9]{24}$"
+            }
+          },
+          "dependencies": {
+            "custom_amount": ["amount"],
+            "frequency": ["recurring"],
+            "first_charge": ["recurring"]
+          }
+        }
+      }
+    },
+    "view": {
+      "parent": "bootstrap-create",
+      "fields": {
+        "/personal_info/email": {
+          "messages": {
+            "invalidEmail": "Invalid email address, e.g. someone@somewhere.com"
+          }
+        },
+        "/billing_address/zip_code": {
+          "messages": {
+            "invalidPattern": "This is not a five-digit (99999) or nine-digit (99999-9999) zip code."
+          }
+        },
+        "/donation_info/stripe_token": {
+          "templates": {
+            "control-text": "<input type=\"hidden\" name=\"stripe_token\"/><div id=\"card-element\"></div><div id=\"card-errors\" class=\"help-block\"></div>"
+          }
+        }
+      }
+    },
+    "options": {
+      "form": {
+        "toggleSubmitValidState": false,
+        "buttons": {
+          "submit": {
+            "title": "Donate"
+          }
+        }
+      },
+      "fields": {
+        "personal_info": {
+          "showMessages": false,
+          "fields": {
+            "email": {
+              "maxMessages": 1
+            },
+            "first_name": {
+              "maxMessages": 1
+            },
+            "last_name": {
+              "maxMessages": 1
+            }
+          }
+        },
+        "billing_address": {
+          "showMessages": false,
+          "fields": {
+            "address_line_1": {
+              "maxMessages": 1,
+              "label": "Billing Address",
+              "placeholder": "Street and number, P.O. box, c/o."
+            },
+            "address_line_2": {
+              "placeholder": "[Optional] Apartment, suite, unit, building, floor, etc."
+            },
+            "city": {
+              "maxMessages": 1
+            },
+            "state": {
+              "maxMessages": 1,
+              "noneLabel": "Select a state...",
+              "hideNone": false,
+              "optionLabels": [
+                "Alabama",
+                "Alaska",
+                "American Samoa",
+                "Arizona",
+                "Arkansas",
+                "Armed Forces Europe",
+                "Armed Forces Pacific",
+                "Armed Forces Americas",
+                "California",
+                "Colorado",
+                "Connecticut",
+                "Delaware",
+                "District of Columbia",
+                "Federated States of Micronesia",
+                "Florida",
+                "Georgia",
+                "Guam",
+                "Hawaii",
+                "Idaho",
+                "Illinois",
+                "Indiana",
+                "Iowa",
+                "Kansas",
+                "Kentucky",
+                "Louisiana",
+                "Maine",
+                "Marshall Islands",
+                "Maryland",
+                "Massachusetts",
+                "Michigan",
+                "Minnesota",
+                "Mississippi",
+                "Missouri",
+                "Montana",
+                "Nebraska",
+                "Nevada",
+                "New Hampshire",
+                "New Jersey",
+                "New Mexico",
+                "New York",
+                "North Carolina",
+                "North Dakota",
+                "Northern Mariana Islands",
+                "Ohio",
+                "Oklahoma",
+                "Oregon",
+                "Palau",
+                "Pennsylvania",
+                "Puerto Rico",
+                "Rhode Island",
+                "South Carolina",
+                "South Dakota",
+                "Tennessee",
+                "Texas",
+                "Utah",
+                "Vermont",
+                "Virgin Islands",
+                "Virginia",
+                "Washington",
+                "West Virginia",
+                "Wisconsin",
+                "Wyoming"
+              ]
+            },
+            "zip_code": {
+              "maxMessages": 1
+            }
+          }
+        },
+        "donation_info": {
+          "showMessages": false,
+          "fields": {
+            "recurring": {
+              "rightLabel": "Make this a recurring donation"
+            },
+            "frequency": {
+              "sort": false,
+              "hideNone": true,
+              "optionLabels": [
+                "Monthly",
+                "Bi-Monthly",
+                "Annually"
+              ]
+            },
+            "first_charge": {
+              "type": "date"
+            },
+            "amount": {
+              "sort": false,
+              "hideNone": true,
+              "type": "radio",
+              "optionLabels": [
+                "$5.00",
+                "$10.00",
+                "$25.00",
+                "Custom Amount"
+              ]
+            },
+            "custom_amount": {
+              "dependencies": {
+                "amount": 0.00
+              },
+              "type": "currency"
+            },
+            "stripe_token": {
+              "showMessages": false
+            }
+          }
+        }
+      }
+    }
+  };
--- page-templates/donate.php
+++ page-templates/donate.php
@@ -57,9 +57,7 @@ $container = get_theme_mod( 'understrap_container_type' );
               </div>
 
               <section id="donation">
-                <div id="donation-form" class="">
-                  <!--  -->
-                </div>
+                <div id="donation-form" class=""></div>
                 <div id="donation-success" hidden="">Thanks for you donation! A receipt has been emailed to you.</div>
               </section>
             </div><!-- .entry-content -->
@@ -74,53 +72,64 @@ $container = get_theme_mod( 'understrap_container_type' );
 <!-- I don't feel great about loading this script right here because, network latency, etc.
 could cause the next script to fail. Maybe set a better checker there? -->
 <script src="https://js.stripe.com/v3/"></script>
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.min.js"></script>
+<script type="text/javascript" src="//code.cloudcms.com/alpaca/1.5.24/bootstrap/alpaca.min.js"></script>
+
 <script type="text/javascript">
-  window.$ = jQuery.noConflict();
+	window.$ = jQuery.noConflict();
 
-	console.log("I'm here and running.");
+	(function(){
 		// This needs to be moved to a secret if possible.
-	var stripe = Stripe('pk_live_4ya4D9KVkQXROVBM4XKgcEgx');
-	var elements = stripe.elements();
-	var card = elements.create('card', {
-		hidePostalCode: true,
-		style: {
-			base: {
-			iconColor: '#666EE8',
-			color: '#31325F',
-			lineHeight: '40px',
-			fontWeight: 300,
-			fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-			fontSize: '15px',
-			height: '20px',
-			'::placeholder': {
-					color: '#CFD7E0',
-				}
-			},
-		}
-	});
-
-	// Use Alpaca styling for Stripe forms.
-	card.addEventListener('change', function(event) {
-		if (event.error) {
-			$('#card-errors').parent().removeClass('alpaca-field-valid')
-				.addClass('has-error')
-				.addClass('alpaca-invalid');
-			$('#card-errors').html('<i class="glyphicon glyphicon-exclamation-sign"></i>&nbsp;')
-				.append(event.error.message).show();
-			$('#donation-form input[name=donation_info_stripe_token]').val('bad');
-		} else {
-			$('#card-errors').parent().removeClass('has-error')
-				.removeClass('alpaca-invalid')
-				.addClass('alpaca-field-valid');
-			$('#card-errors').text('').hide();
-			if (event.complete) {
-				$('#donation-form input[name=donation_info_stripe_token]').val('tok_000000000000000000000000');
+		console.log("I'm starting up...")
+		var stripe = Stripe('pk_test_ngn8Hld7TMTDi4jPc48D20HU');
+		var elements = stripe.elements();
+		var card = elements.create('card', {
+			hidePostalCode: true,
+			style: {
+				base: {
+				iconColor: '#666EE8',
+				color: '#31325F',
+				lineHeight: '40px',
+				fontWeight: 300,
+				fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+				fontSize: '15px',
+				height: '20px',
+				'::placeholder': {
+						color: '#CFD7E0',
+					}
+				},
 			}
-		}
-	});
+		});
 
-	// Get Alpaca-ready field to build form.
-	$.getJSON( "/donation/donate", function(schema) {
+		// Use Alpaca styling for Stripe forms.
+		card.addEventListener('change', function(event) {
+			if (event.error) {
+				$('#card-errors').parent().removeClass('alpaca-field-valid')
+					.addClass('has-error')
+					.addClass('alpaca-invalid');
+				$('#card-errors').html('<i class="glyphicon glyphicon-exclamation-sign"></i>&nbsp;')
+					.append(event.error.message).show();
+				$('#donation-form input[name=donation_info_stripe_token]').val('bad');
+			} else {
+				$('#card-errors').parent().removeClass('has-error')
+					.removeClass('alpaca-invalid')
+					.addClass('alpaca-field-valid');
+				$('#card-errors').text('').hide();
+				if (event.complete) {
+					$('#donation-form input[name=donation_info_stripe_token]').val('tok_000000000000000000000000');
+				}
+			}
+		});
+
+
+
+		// Get Alpaca-ready field to build form.
+		// Using my silly test file  . . .
+		//$.getJSON( "/donation/donate", function(schema) {
+
+		// Self invoking function to stub out JSON call.
+		var schema =  window.schemaTest;
+		console.log(schema);
 		var now = new Date();
 		schema.options.fields.donation_info.fields.first_charge = {
 			"picker": {
@@ -150,140 +159,170 @@ could cause the next script to fail. Maybe set a better checker there? -->
 			} else {
 				callback({'status': true});
 			}
-    };
+		};
 
-    schema.postRender = function(control) {
+		schema.postRender = function(control) {
+			console.log(control);
 			$('#donation-form button[type=submit]').addClass('ladda-button')
 				.addClass('btn-primary')
 				.removeClass('btn-default')
 				.attr('data-style', 'expand-left');
-      card.mount('#card-element');
-    };
-    $("#donation-form").alpaca(schema);
-  });
+			if ($("#card-element").length) {
+				card.mount('#card-element');
+			} else {
+				console.log("Couldn't find it . . .")
+			}
+			addRadioHandlers();
+		};
+		console.log(schema);
 
-	$('#donation-form').submit(function(event) {
-    event.preventDefault();
-    Ladda.create(document.querySelector('#donation-form button[type=submit]')).start();
-    var address = {
-			address_line_1: $('#donation-form input[name=billing_address_address_line_1]').val(),
-			address_line_2: $('#donation-form input[name=billing_address_address_line_2]').val(),
-			city: $('#donation-form input[name=billing_address_city]').val(),
-			state: $('#donation-form select[name=billing_address_state]').val(),
-			zip_code: $('#donation-form input[name=billing_address_zip_code]').val(),
-    };
-    $.ajax({
-			type: 'POST',
-			url: '/usps/validate',
-			data: JSON.stringify(address),
-			contentType: "application/json; charset=utf-8",
-			dataType: "json",
-			success: process_validated_address,
-			error: function(jqxhr) {
+		$("#donation-form").alpaca(schema);
+		//}); //When going live. Remove self-invoking part.
+
+		//Function to add yellow BG to inputs when selected;
+		function addRadioHandlers(){
+			var $radios = $("#donation-form").find(".alpaca-field-radio");
+			console.log("Radios", $radios);
+
+			$radios.find('input[type="radio"]').hide();
+
+			$radios.find('label').click(function(){
+				var $this = $(this);
+				var $siblingLabels = $this.closest('.alpaca-field-radio').find('.radio');
+				$siblingLabels.removeClass('checked');
+				$this.closest('.radio').addClass('checked');
+			});
+		}
+
+
+		$('#donation-form').submit(function(event) {
+			event.preventDefault();
+			// Ladda.create(document.querySelector('#donation-form button[type=submit]')).start();
+			var address = {
+				address_line_1: $('#donation-form input[name=billing_address_address_line_1]').val(),
+				address_line_2: $('#donation-form input[name=billing_address_address_line_2]').val(),
+				city: $('#donation-form input[name=billing_address_city]').val(),
+				state: $('#donation-form select[name=billing_address_state]').val(),
+				zip_code: $('#donation-form input[name=billing_address_zip_code]').val(),
+			};
+			console.log(address);
+			$.ajax({
+				type: 'POST',
+				url: '/usps/validate',
+				data: JSON.stringify(address),
+				contentType: "application/json; charset=utf-8",
+				dataType: "json",
+				success: process_validated_address,
+				error: function(jqxhr) {
+					var errorElement = document.getElementById('error_modal_text');
+					errorElement.textContent = result.error;
+					$('#error_modal').modal('show');
+					// Ladda.stopAll();
+				}
+			});
+		});
+
+		// TODO: Remove from global scope.
+		var address;
+		function process_validated_address(data) {
+			console.log("Successfully ran processor.")
+			if (!($('#donation-form input[name=billing_address_address_line_1]').val() != data['address_line_1'] ||
+				$('#donation-form input[name=billing_address_address_line_2]').val() != data['address_line_2'] ||
+				$('#donation-form input[name=billing_address_city]').val() != data['city'] ||
+				$('#donation-form select[name=billing_address_state]').val() != data['state'] ||
+				$('#donation-form input[name=billing_address_zip_code]').val() != data['zip_code'])) {
+					address_selected();
+					return;
+			}
+			$('#verify_address').text(data["address_line_1"] + " " + data["address_line_2"] + "\n" + data["city"] + ", " + data["state"] + "  " + data["zip_code"]);
+			if (data.usps_extra.ReturnText) {
+				$('#usps_extra').text("USPS WARNING:\n" + data.usps_extra.ReturnText).show();
+			} else {
+				$('#usps_extra').hide();
+			}
+			$('#entered_address').text(
+				$('#donation-form input[name=billing_address_address_line_1]').val() + " " +
+				$('#donation-form input[name=billing_address_address_line_2]').val() + "\n" +
+				$('#donation-form input[name=billing_address_city]').val() + ", " +
+				$('#donation-form select[name=billing_address_state]').val() + "  " +
+				$('#donation-form input[name=billing_address_zip_code]').val()
+			);
+
+			$('#address_validate_modal').modal('show');
+			address = data;
+		}
+		$('#use_validated_address').click(function(event) {
+			$('#donation-form input[name=billing_address_address_line_1]').val(address["address_line_1"]);
+			$('#donation-form input[name=billing_address_address_line_2]').val(address["address_line_2"]);
+			$('#donation-form input[name=billing_address_city]').val(address["city"]);
+			$('#donation-form select[name=billing_address_state]').val(address["state"]);
+			$('#donation-form input[name=billing_address_zip_code]').val(address["zip_code"]);
+			address_selected();
+		});
+
+		$('#use_entered_address').click(address_selected);
+
+		//TODO: Remove
+		$('#cancel_address_validation').click(function(event) {
+
+		});
+
+		function address_selected() {
+			var stripe_info = {
+				name: $('#donation-form input[name=personal_info_first_name]').val() + ' ' + $('#donation-form input[name=personal_info_last_name]').val(),
+				address_line1: $('#donation-form input[name=billing_address_address_line_1]').val(),
+				address_line2: $('#donation-form input[name=billing_address_address_line_2]').val(),
+				address_city: $('#donation-form input[name=billing_address_city]').val(),
+				address_state: $('#donation-form select[name=billing_address_state]').val(),
+				address_zip: $('#donation-form input[name=billing_address_zip_code]').val(),
+			};
+			stripe.createToken(card, stripe_info).then(process_stripe_token_creation);
+		}
+
+		function process_stripe_token_creation(result) {
+			if (result.token) {
+				$('#donation-form input[name=donation_info_stripe_token]').val(result.token.id);
+				$.ajax({
+					type: 'POST',
+					url: '/donation/donate',
+					data: JSON.stringify($("#donation-form").alpaca("get").getValue()),
+					contentType: "application/json; charset=utf-8",
+					dataType: "json",
+					success: process_donation,
+					error: function(jqxhr) {
+						var errorElement = document.getElementById('error_modal_text');
+						if (jqxhr.responseJSON) {
+							errorElement.textContent = jqxhr.responseJSON.error;
+						} else {
+							errorElement.textContent = 'Unknown error.';
+						}
+						$('#error_modal').modal('show');
+						// Ladda.stopAll();
+					}
+				});
+			} else if (result.error) {
+				var errorElement = document.getElementById('error_modal_text');
+				errorElement.textContent = result.error.message;
+				$('#error_modal').modal('show');
+				// Ladda.stopAll();
+			}
+		}
+
+		function process_donation(result) {
+			if (result.error) {
 				var errorElement = document.getElementById('error_modal_text');
 				errorElement.textContent = result.error;
 				$('#error_modal').modal('show');
-				Ladda.stopAll();
-      }
-    });
-  });
-
-	// TODO: Remove from global scope.
-	var address;
-  function process_validated_address(data) {
-    if (!($('#donation-form input[name=billing_address_address_line_1]').val() != data['address_line_1'] ||
-			$('#donation-form input[name=billing_address_address_line_2]').val() != data['address_line_2'] ||
-			$('#donation-form input[name=billing_address_city]').val() != data['city'] ||
-			$('#donation-form select[name=billing_address_state]').val() != data['state'] ||
-			$('#donation-form input[name=billing_address_zip_code]').val() != data['zip_code'])) {
-        address_selected();
-        return;
-    }
-    $('#verify_address').text(data["address_line_1"] + " " + data["address_line_2"] + "\n" + data["city"] + ", " + data["state"] + "  " + data["zip_code"]);
-    if (data.usps_extra.ReturnText) {
-      $('#usps_extra').text("USPS WARNING:\n" + data.usps_extra.ReturnText).show();
-    } else {
-      $('#usps_extra').hide();
-    }
-    $('#entered_address').text(
-			$('#donation-form input[name=billing_address_address_line_1]').val() + " " +
-			$('#donation-form input[name=billing_address_address_line_2]').val() + "\n" +
-			$('#donation-form input[name=billing_address_city]').val() + ", " +
-			$('#donation-form select[name=billing_address_state]').val() + "  " +
-			$('#donation-form input[name=billing_address_zip_code]').val()
-		);
-
-    $('#address_validate_modal').modal('show');
-    address = data;
-  }
-	$('#use_validated_address').click(function(event) {
-		$('#donation-form input[name=billing_address_address_line_1]').val(address["address_line_1"]);
-		$('#donation-form input[name=billing_address_address_line_2]').val(address["address_line_2"]);
-		$('#donation-form input[name=billing_address_city]').val(address["city"]);
-		$('#donation-form select[name=billing_address_state]').val(address["state"]);
-		$('#donation-form input[name=billing_address_zip_code]').val(address["zip_code"]);
- 	  address_selected();
-  });
-
-	$('#use_entered_address').click(address_selected);
-
-	$('#cancel_address_validation').click(function(event) {
-		Ladda.stopAll();
-	});
-
-	function address_selected() {
-		var stripe_info = {
-			name: $('#donation-form input[name=personal_info_first_name]').val() + ' ' + $('#donation-form input[name=personal_info_last_name]').val(),
-			address_line1: $('#donation-form input[name=billing_address_address_line_1]').val(),
-			address_line2: $('#donation-form input[name=billing_address_address_line_2]').val(),
-			address_city: $('#donation-form input[name=billing_address_city]').val(),
-			address_state: $('#donation-form select[name=billing_address_state]').val(),
-			address_zip: $('#donation-form input[name=billing_address_zip_code]').val(),
-		};
-		stripe.createToken(card, stripe_info).then(process_stripe_token_creation);
-	}
-
-	function process_stripe_token_creation(result) {
-		if (result.token) {
-			$('#donation-form input[name=donation_info_stripe_token]').val(result.token.id);
-			$.ajax({
-				type: 'POST',
-				url: '/donation/donate',
-				data: JSON.stringify($("#donation-form").alpaca("get").getValue()),
-				contentType: "application/json; charset=utf-8",
-				dataType: "json",
-				success: process_donation,
-				error: function(jqxhr) {
-					var errorElement = document.getElementById('error_modal_text');
-					if (jqxhr.responseJSON) {
-						errorElement.textContent = jqxhr.responseJSON.error;
-					} else {
-						errorElement.textContent = 'Unknown error.';
-					}
-					$('#error_modal').modal('show');
-					Ladda.stopAll();
-				}
-			});
-		} else if (result.error) {
-			var errorElement = document.getElementById('error_modal_text');
-			errorElement.textContent = result.error.message;
-			$('#error_modal').modal('show');
-			Ladda.stopAll();
+				// Ladda.stopAll();
+			} else {
+				$('#donation-form').hide();
+				$('#donation-success').show();
+				// Ladda.stopAll();
+			}
 		}
-	}
+	})();
 
-	function process_donation(result) {
-		if (result.error) {
-			var errorElement = document.getElementById('error_modal_text');
-			errorElement.textContent = result.error;
-			$('#error_modal').modal('show');
-			Ladda.stopAll();
-		} else {
-			$('#donation-form').hide();
-			$('#donation-success').show();
-			Ladda.stopAll();
-		}
-	}
+
 </script>
 <style type="text/css">
 .StripeElement {
