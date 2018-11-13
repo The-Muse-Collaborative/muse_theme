19d6e425e64dc8c79ef63bbdb36acf5c28fdf625
Michael Zier <michael@themusecollaborative.org>
2018-11-12 21:17:30 -0500

    Divided and sorted functions for admin bar

--- inc/admin-bar.php
+++ inc/admin-bar.php
@@ -5,39 +5,74 @@
  * @package understrap
  */
 
-//removes icon from left admin bar
- function remove_left_menus(){
+//determines order of left admin bar icons
+function custom_menu_order($menu_ord) {
+  if (!$menu_ord) return true;
 
-   remove_menu_page( 'edit-comments.php' );          // Comments
-   //remove_menu_page( 'themes.php' );               // Appearance
-
+     return array(
+         'index.php',                  // Dashboard
+         'CiviCRM',                    // CiviCRM
+         'upload.php',                 // Media
+         'separator1',                 // First separator
+         'edit.php?post_type=page',    // Pages
+         'edit.php',                   // Posts
+         'edit.php?post_type=albums',  // Albums
+         'edit.php?post_type=articles',// Articles
+         'edit.php?post_type=artists', // Artists
+         'edit.php?post_type=bios',    // Bios
+         'separator2',                 // Second separator
+         'themes.php',                 // Appearance
+         'plugins.php',                // Plugins
+         'options-general.php',        // Settings
+         'tools.php',                  // Tools
+         'users.php',                  // Users
+         'separator-last',             // Last separator
+     );
  }
- add_action( 'admin_menu', 'remove_left_menus' );
+ add_filter('custom_menu_order', 'custom_menu_order'); // Activate custom_menu_order
+ add_filter('menu_order', 'custom_menu_order');
 
-//removes icon from top admin bar
- function remove_top_menues(){
-         global $wp_admin_bar;
-         $wp_admin_bar->remove_menu('comments');       // Comments
-         $wp_admin_bar->remove_menu('wp-logo');        // Wordpress Home
-         if( !current_user_can( 'administrator' ) ):
-           $wp_admin_bar->remove_menu('my-account');     // My account
-           $wp_admin_bar->remove_menu('new-content');    // + New item
-         endif;
+////// ALL USER REMOVALS //////
+
+//removes items from left admin bar
+function remove_left_menus() {
+remove_menu_page( 'edit-comments.php' );          // Comments
+//remove_menu_page( 'themes.php' );               // Appearance
+}
+add_action( 'admin_menu', 'remove_left_menus' );
+
+//removes items from top admin bar
+function remove_top_menus() {
+  global $wp_admin_bar;
+  $wp_admin_bar->remove_menu('comments');       // Comments
+  $wp_admin_bar->remove_menu('wp-logo');        // Wordpress Home
  }
- add_action( 'wp_before_admin_bar_render', 'remove_top_menues' );
+ add_action( 'wp_before_admin_bar_render', 'remove_top_menus' );
 
-//removes toolbar items for non-admins
- function restrict_menu_items() {
-    if( !current_user_can( 'administrator' ) ):
-        remove_menu_page( 'edit.php?post_type=bios' );
-        remove_menu_page( 'tools.php' );
-        remove_menu_page( 'profile.php' );
-        remove_menu_page( 'edit.php' );
+////// SPECIFIC USER REMOVALS //////
+
+//removes items from left admin bar for non-admins
+function remove_left_menu_items() {
+  if( !current_user_can( 'administrator' ) ):
+    remove_menu_page( 'edit.php?post_type=bios' );
+    remove_menu_page( 'tools.php' );
+    remove_menu_page( 'profile.php' );
+    remove_menu_page( 'edit.php' );
     endif;
 }
-add_action( 'admin_menu', 'restrict_menu_items' );
+add_action( 'admin_menu', 'remove_left_menu_items' );
 
-// Remove dashboard widgets for non-admins
+//removes items from top admin bar for non-admins
+function remove_top_menu_items() {
+  global $wp_admin_bar;
+  if( !current_user_can( 'administrator' ) ):
+    $wp_admin_bar->remove_menu('my-account');     // My account
+    $wp_admin_bar->remove_menu('new-content');    // + New item
+  endif;
+}
+add_action( 'wp_before_admin_bar_render', 'remove_top_menu_items' );
+
+//remove dashboard widgets for non-admins
 function remove_dashboard_meta() {
 	if( !current_user_can( 'administrator' ) ):
 		remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' ); //wordpress news
@@ -46,30 +81,3 @@ function remove_dashboard_meta() {
   endif;
 }
 add_action( 'admin_init', 'remove_dashboard_meta' );
-
-//determines order of left admin bar icons
- function custom_menu_order($menu_ord) {
-    if (!$menu_ord) return true;
-
-    return array(
-        'index.php',                  // Dashboard
-        'CiviCRM',                    // CiviCRM
-        'upload.php',                 // Media
-        'separator1',                 // First separator
-        'edit.php?post_type=page',    // Pages
-        'edit.php',                   // Posts
-        'edit.php?post_type=albums',  // Albums
-        'edit.php?post_type=articles',// Articles
-        'edit.php?post_type=artists', // Artists
-        'edit.php?post_type=bios',    // Bios
-        'separator2',                 // Second separator
-        'themes.php',                 // Appearance
-        'plugins.php',                // Plugins
-        'options-general.php',        // Settings
-        'tools.php',                  // Tools
-        'users.php',                  // Users
-        'separator-last',             // Last separator
-    );
-}
-add_filter('custom_menu_order', 'custom_menu_order'); // Activate custom_menu_order
-add_filter('menu_order', 'custom_menu_order');
